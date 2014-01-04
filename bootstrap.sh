6523ec337690650440c29b81e9b4b31960856190
TylerBackman <tyler.backman@ucr.edu>
2014-01-03 18:29:01 -0800

    added R compile and package install

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,13 @@
 # setup base operating system
 apt-get update
 apt-get upgrade -y
+apt-get install -y gfortran 
+apt-get install -y build-essential
+apt-get install -y x11-common 
+apt-get install -y libx11-dev 
+apt-get install -y xserver-xorg-dev 
+apt-get install -y xserver-xorg 
+apt-get install -y libxt-dev 
 
 # install common bioinfo/cheminfo packages
 apt-get install -y blast2
@@ -13,7 +20,7 @@ mkdir /tmp/compileR
 cd /tmp/compileR
 wget http://cran.cnr.berkeley.edu/src/base/R-3/R-3.0.2.tar.gz
 tar xvfz R-3.0.2.tar.gz
-cd /tmp/R-3.0.2
+cd R-3.0.2
 ./configure
 make
 make check
@@ -23,7 +30,18 @@ make install-info
 cd ~/ 
 rm -rf /tmp/compileR
 
+# install prereqs for building R packages
+apt-get install -y curl libcurl4-openssl-dev
+apt-get install -y libgsl0-dev libgsl0ldbl
+apt-get install -y libxml2 libxml2-dev 
+
 # install R packages
+printf "source(\"http://bioconductor.org/biocLite.R\")
+biocLite()
+biocLite(c(\"ShortRead\", \"Biostrings\", \"IRanges\", \"BSgenome\", \"rtracklayer\", \"biomaRt\",
+\"ChemmineR\", \"fmcsR\", \"bioassayR\", \"cellHTS2\", \"RCurl\", \"ape\", \"eiR\"))
+" | R --slave
 
 # clean up
+R INSTALL --clean
 apt-get clean
