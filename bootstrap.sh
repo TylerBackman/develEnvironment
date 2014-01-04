741e9eb807b046e390d37b0fdc33521dd65d09d0
TylerBackman <tyler.backman@ucr.edu>
2014-01-03 18:57:34 -0800

    added openbabel

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,9 @@ apt-get install -y libx11-dev
 apt-get install -y xserver-xorg-dev 
 apt-get install -y xserver-xorg 
 apt-get install -y libxt-dev 
+# tools for compiling obabel
+apt-get install -y cmake 
+apt-get install -y libeigen3-dev zlib1g-dev libeigen2-dev libcairo2-dev python-dev
 
 # install common bioinfo/cheminfo packages
 apt-get install -y blast2
@@ -41,6 +44,19 @@ biocLite()
 biocLite(c(\"ShortRead\", \"Biostrings\", \"IRanges\", \"BSgenome\", \"rtracklayer\", \"biomaRt\",
 \"ChemmineR\", \"fmcsR\", \"bioassayR\", \"cellHTS2\", \"RCurl\", \"ape\", \"eiR\"))
 " | R --slave
+
+# compile and install openbabel
+mkdir /tmp/compileOB
+cd /tmp/compileOB
+wget -O ob.tgz http://downloads.sourceforge.net/project/openbabel/openbabel/2.3.2/openbabel-2.3.2.tar.gz?r=http%3A%2F%2Fopenbabel.org%2Fwiki%2FGet_Open_Babel&ts=1388802736&use_mirror=superb-dca2
+tar xvfz ob.tgz
+mkdir build
+cd build
+cmake ../openbabel-2.3.2 -DPYTHON_BINDINGS=ON
+make -j2
+make install
+cd ~/
+rm -rf /tmp/compileOB
 
 # clean up
 R INSTALL --clean
