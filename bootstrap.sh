a18a6b2931c7cfc63dd46509624b82ac56fd8ee6
TylerBackman <tbackman@ucr.edu>
2014-05-12 16:17:13 -0700

    updated R

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,9 +23,9 @@ apt-get install -y hmmer
 # compile and install R
 mkdir /tmp/compileR
 cd /tmp/compileR
-wget http://cran.cnr.berkeley.edu/src/base/R-3/R-3.0.2.tar.gz
-tar xvfz R-3.0.2.tar.gz
-cd R-3.0.2
+wget http://cran.cnr.berkeley.edu/src/base/R-3/R-3.1.0.tar.gz
+tar xvfz R-3.1.0.tar.gz
+cd R-3.1.0
 ./configure
 make
 make check
